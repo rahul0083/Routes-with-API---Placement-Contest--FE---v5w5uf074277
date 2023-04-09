c41aea40ad3e1b88fa99088c55b2fd5c0c44fd8f
Rahul Singh <rahulme0083@gmail.com>
2023-04-09 16:29:25 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,11 @@ const App = () => {
   return (
     <div id="main">
       <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/top-10" element={<Top />} />
+        <Route exact path="/contact" element={<Contact />} />
+      </Routes>
       <div className='container'></div>
     </div>
   )
--- src/components/Top.js
+++ src/components/Top.js
@@ -2,18 +2,29 @@ import React, { useState, useEffect } from 'react';
 
 function Top() {
     const [cryptoArr, setCryptoArr] = useState([]);
+   
+    useEffect(()=>{
+         
+       const fetchdata=async()=>{
+        const response=await fetch("https://api.coinlore.net/api/tickers/");
+        const data=await response.json();
+        setCryptoArr(data.data); 
+        
+       };
+       fetchdata();
 
+    },[])
     return (
         <div className='top-container'>
             <h1>Top 10 Global Crypto Coins</h1>
             <div className='display-container'>
-
-                <div className='coin-container'>
-                    <p className='rank-para'>Rank #rank</p>
-                    <h3 className='coin-symbol'>name (symbol)</h3>
-                    <p className='price-para'>Price: price_usd</p>
+              {  cryptoArr.map((data)=>{
+                <div className='coin-container' key={data.id}>
+                    <p className='rank-para'> {data.rank}</p>
+                    <h3 className='coin-symbol'>{data.symbol}</h3>
+                    <p className='price-para'>{data.price_usd}</p>
                 </div>
-
+})}
             </div>
         </div>
     )
