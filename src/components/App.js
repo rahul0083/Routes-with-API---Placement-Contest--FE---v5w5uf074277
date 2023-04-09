a476ee674d734b5d541569dce8888a8e97397bef
Rahul Singh <rahulme0083@gmail.com>
2023-04-09 17:06:01 +0530

    update2

--- src/components/App.js
+++ src/components/App.js
@@ -11,12 +11,13 @@ const App = () => {
   return (
     <div id="main">
       <Navbar />
+     
+      <div className='container'>\
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/top-10" element={<Top />} />
         <Route exact path="/contact" element={<Contact />} />
-      </Routes>
-      <div className='container'></div>
+      </Routes></div>
     </div>
   )
 }
